785c8369314b72bc9dcac0538f2b61fb83337654
Kamau-sam <skamau.6411@gmail.com>
2024-03-28 22:47:37 +0300

    Completed test

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solutions in this file
+const names = ["Guadalupe", "Ollie", "Aki"];
+const event = "gift";
+function writeCards(names, event) {
+  let thankMessage = [];
+  for (let i = 0; i < names.length; i++) {
+    thankMessage.push(
+      `Thank you, ${names[i]}, for the wonderful ${event} gift!`
+    );
+  }
+  return thankMessage;
+}
+
+function countDown(startingNumber) {
+  for (let i = startingNumber; i >= 0; i--) {
+    console.log(i);
+  }
+}
